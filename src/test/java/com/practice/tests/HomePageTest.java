f497863bc31c05774277a6981cf038448dd564ea
dipteeanutham <dipteepatil2017@gmail.com>
2020-10-21 17:57:30 +0530

    first git commit for my selenium code

---
+++ src/test/java/com/practice/tests/HomePageTest.java
@@ -0,0 +1,11 @@
+package com.practice.tests;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+	@Test
+	public void testMethod() {
+		System.out.println("HomePageTest - testMethod");
+	}
+}
